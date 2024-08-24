937ab952fbd4c9cc53fbbc9b2d3461fe4b9e5300
Ashish kumar <ashishkumar082018@gmail.com>
2024-08-24 18:16:33 +0530

    Rendering Users List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+const users = [];
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={users}></UsersList>
     </div>
   );
 }
